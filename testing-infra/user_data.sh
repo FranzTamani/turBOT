036fe850c16837b5a0974c3d14962046e7fa62c9
Franz Tamani <franzjordaneal@gmail.com>
2021-08-25 19:27:10 +1000

    add python to installed items on the instance

--- testing-infra/user_data.sh
+++ testing-infra/user_data.sh
@@ -3,4 +3,4 @@
 wget -O - https://download.grammatech.com/gtirb/files/apt-repo/conf/apt.gpg.key | apt-key add -
 echo "deb https://download.grammatech.com/gtirb/files/apt-repo focal stable"| tee -a /etc/apt/sources.list
 apt update -y
-apt install ddisasm git g++ -y
+apt install ddisasm git g++ python3 -y
