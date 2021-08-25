3b7deeda643e9a8e6cf0f2a981c0b7231dffed6e
FranzTamani <37327603+FranzTamani@users.noreply.github.com>
2021-08-25 20:25:30 +1000

    Configure instance for testing (#6)
    * add python to installed items on the instance
    
    * install bot test CLI to instance
    
    * fix typos
    
    * cleanup
    
    * add test files

--- testing-infra/user_data.sh
+++ testing-infra/user_data.sh
@@ -3,4 +3,20 @@
 wget -O - https://download.grammatech.com/gtirb/files/apt-repo/conf/apt.gpg.key | apt-key add -
 echo "deb https://download.grammatech.com/gtirb/files/apt-repo focal stable"| tee -a /etc/apt/sources.list
 apt update -y
-apt install ddisasm git g++ -y
+apt install ddisasm git g++ python3 -y
+
+git clone https://github.com/FranzTamani/TestCLI.git
+
+cd TestCLI && \
+chmod +x ./setup.sh && \
+./setup.sh
+
+export PATH="/TestCLI:$PATH"
+
+mkdir /home/ubuntu/Test
+mkdir /home/ubuntu/TestOutput
+touch /home/ubuntu/Test/default.o
+touch /home/ubuntu/Test/disassemble.o
+touch /home/ubuntu/Test/reassemble.asm
+touch /home/ubuntu/Test/obfuscate.asm
+
