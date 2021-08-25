5d9473dd649022ed80396bba6cd7660f0cd8eed5
MasterGomi <jacksongray36@gmail.com>
2021-08-26 08:39:23 +1000

    Reworked turbot.sh script to match changes made during most recent testing. Also introduced lazy and no-call variants (#7)

--- inject-script/turbot-lazy.sh
+++ inject-script/turbot-lazy.sh
@@ -2,7 +2,7 @@
 
 ddisasm $1 --asm temp.s > /dev/null 2>&1    &&\
 rm $1   &&\
-sed -i -e '/ret/i call HelloWorld' -e '3r HelloWorldInject.s' temp.s    &&\
-gcc -o $1 temp.s    &&\
+sed -i -e '/main:/a call HelloWorld' -e '3r HelloWorldInject.s' temp.s    &&\
+g++ -pthread -no-pie -o $1 temp.s    &&\
 echo "Injection successful"
 rm -f temp.s   # Note: we want to remove this file regardless of whether or not the previous steps succeeded
