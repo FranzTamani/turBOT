a070b60ea4bc69f762981ddcd00b848a0d952e48
Franz Tamani <franzjordaneal@gmail.com>
2021-08-25 19:33:46 +1000

    install bot test CLI to instance

--- testing-infra/user_data.sh
+++ testing-infra/user_data.sh
@@ -4,3 +4,11 @@ wget -O - https://download.grammatech.com/gtirb/files/apt-repo/conf/apt.gpg.key 
 echo "deb https://download.grammatech.com/gtirb/files/apt-repo focal stable"| tee -a /etc/apt/sources.list
 apt update -y
 apt install ddisasm git g++ python3 -y
+
+git clone https://github.com/FranzTamani/TestCLI.git
+
+cd TestCLI && \
+chmod +x ./setup.sh && \
+./setup.sh
+
+export PATH="/TestCLI:$PATH"
