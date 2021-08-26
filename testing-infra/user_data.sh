4b008deb0026d5e3080ae53149d1db8cf61959c5
Daniel Coady <me@pondo.dev>
2021-08-26 22:48:45 +1000

    remove ddisasm and g++ from installs in deploy script

--- testing-infra/user_data.sh
+++ testing-infra/user_data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-wget -O - https://download.grammatech.com/gtirb/files/apt-repo/conf/apt.gpg.key | apt-key add -
-echo "deb https://download.grammatech.com/gtirb/files/apt-repo focal stable"| tee -a /etc/apt/sources.list
 apt update -y
-apt install ddisasm git g++ python3 -y
+apt install git python3 -y
 
 git clone https://github.com/FranzTamani/turBOT.git
 
