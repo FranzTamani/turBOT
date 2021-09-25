8be7478e8b8d6469198d62d1cade91426d0e8b23
Jackson <jacksongray36@gmail.com>
2021-09-25 13:33:24 +1000

    Used the standalone syslogs .s file to make an inject file. Put all payloads into a payloads sub-folder in CLI/. Tested and confirmed functionality for logTamperInject.s and multiple payload injection for turbot.py

--- inject-script/HelloWorldInject.s
+++
@@ -1,9 +0,0 @@
-hello:      .asciz "Hello World\n"
-            helloLen = .- hello
-HelloWorld:
-            mov rax, 1
-            mov rdi, 1
-            lea rsi, [hello + rip]
-            mov rdx, helloLen
-            syscall
-            ret
--- inject-script/turbot-lazy.sh
+++
@@ -1,8 +0,0 @@
-# $1 should be target program to disassemble
-
-ddisasm $1 --asm temp.s > /dev/null 2>&1    &&\
-rm $1   &&\
-sed -i -e '/main:/a call HelloWorld' -e '3r HelloWorldInject.s' temp.s    &&\
-g++ -pthread -no-pie -o $1 temp.s    &&\
-echo "Injection successful"
-rm -f temp.s   # Note: we want to remove this file regardless of whether or not the previous steps succeeded
