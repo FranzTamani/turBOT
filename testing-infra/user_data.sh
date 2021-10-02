5f51524780cf839969e1141145034a61ddda782b
Franz Tamani <franzjordaneal@gmail.com>
2021-10-02 15:44:58 +1000

    update user data to use new installer file

--- testing-infra/user_data.sh
+++ testing-infra/user_data.sh
@@ -6,16 +6,9 @@ apt install git python3 -y
 git clone https://github.com/FranzTamani/turBOT.git
 
 cd turBOT/CLI && \
-chmod +x ./setup.sh && \
-./setup.sh && \
+chmod +x ./install.sh && \
+./install.sh && \
 cp turbot /usr/bin
-
-mkdir /home/ubuntu/Test
-mkdir /home/ubuntu/TestOutput
-touch /home/ubuntu/Test/default.o
-touch /home/ubuntu/Test/disassemble.o
-touch /home/ubuntu/Test/reassemble.asm
-touch /home/ubuntu/Test/obfuscate.asm
 
 chown -R ubuntu:ubuntu /home/ubuntu
 
